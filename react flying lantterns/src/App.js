b9f49c4e1ed6732a7268ba6e5939f26d7ef449fb
Subhajit-Dutta <146566101+Subhaji-Dutta@users.noreply.github.com>
2024-01-16 17:37:17 +0530

    Add files via upload

---
+++ react flying lantterns/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+function App() {
+  return (
+    <div className='container'>
+      <div className='bubbles'>
+        <span style ={{"--bg": 11}}></span>
+        <span style ={{"--bg": 12}}></span> 
+        <span style ={{"--bg": 24}}></span>
+        <span style ={{"--bg": 10}}></span> 
+        <span style ={{"--bg": 14}}></span>
+        <span style ={{"--bg": 23}}></span>  
+        <span style ={{"--bg": 18}}></span>
+        <span style ={{"--bg": 16}}></span> 
+        <span style ={{"--bg": 19}}></span>
+        <span style ={{"--bg": 20}}></span> 
+        <span style ={{"--bg": 22}}></span>
+        <span style ={{"--bg": 25}}></span>  
+        <span style ={{"--bg": 18}}></span>
+        <span style ={{"--bg": 21}}></span> 
+        <span style ={{"--bg": 15}}></span>
+        <span style ={{"--bg": 13}}></span> 
+        <span style ={{"--bg": 26}}></span>
+        <span style ={{"--bg": 17}}></span>  
+        <span style ={{"--bg": 13}}></span>
+        <span style ={{"--bg": 28}}></span> 
+        <span style ={{"--bg": 26}}></span>
+        <span style ={{"--bg": 16}}></span> 
+        <span style ={{"--bg": 24}}></span>
+        <span style ={{"--bg": 27}}></span>  
+        <span style ={{"--bg": 19}}></span>
+        <span style ={{"--bg": 20}}></span> 
+        <span style ={{"--bg": 22}}></span>
+        <span style ={{"--bg": 25}}></span>  
+        <span style ={{"--bg": 18}}></span>
+        <span style ={{"--bg": 21}}></span> 
+        <span style ={{"--bg": 15}}></span>
+        <span style ={{"--bg": 13}}></span> 
+        <span style ={{"--bg": 26}}></span>
+        <span style ={{"--bg": 17}}></span>  
+        <span style ={{"--bg": 13}}></span>
+        <span style ={{"--bg": 28}}></span> 
+        <span style ={{"--bg": 26}}></span>
+        <span style ={{"--bg": 16}}></span> 
+        <span style ={{"--bg": 24}}></span>
+        <span style ={{"--bg": 27}}></span> 
+        <span style ={{"--bg": 26}}></span>
+        <span style ={{"--bg": 16}}></span> 
+        <span style ={{"--bg": 24}}></span>
+        <span style ={{"--bg": 27}}></span>  
+        <span style ={{"--bg": 19}}></span>
+        <span style ={{"--bg": 20}}></span> 
+        <span style ={{"--bg": 22}}></span>
+        <span style ={{"--bg": 25}}></span>  
+        <span style ={{"--bg": 18}}></span>
+        <span style ={{"--bg": 21}}></span> 
+      </div>
+      <div className='title'>Coming Soon</div>
+    </div>
+    
+  );
+}
+
+export default App;
